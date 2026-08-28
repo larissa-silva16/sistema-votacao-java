eeeba49e2428275804ab66758a430cbf0f087efd
larisa Pereira Martins da Silva <larisss.11736372@aluno.mg.gov.br>
2026-08-28 15:28:46 -0300

    Sistema de votacao

---
+++ src/SistemaVotacao.java
@@ -0,0 +1,11 @@
+public class SistemaVotacao {
+}
+import java.util.Scanner;
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de votação iniciado.");
+        scanner.close();
+    }
+}
