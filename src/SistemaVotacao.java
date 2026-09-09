a642718bcc306dee49d7e7fb58ca69048821696e
larisa Pereira Martins da Silva <larisss.11736372@aluno.mg.gov.br>
2026-09-09 10:25:51 -0300

    feat: validar quantidade de candidatos

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -69,4 +69,22 @@ public class SistemaVotacao {
             scanner.nextLine();
         }
     }
+    static void cadastrarCandidatos() {
+        if (quantidadeCandidatos > 0) {
+            System.out.println("Os candidatos já foram cadastrados.");
+            return;
+        }
+
+        int quantidade;
+
+        do {
+            quantidade = lerInteiro(
+                    "Quantidade de candidatos entre 1 e 5: "
+            );
+
+            if (quantidade < 1 || quantidade > MAX_CANDIDATOS) {
+                System.out.println("Quantidade inválida.");
+            }
+        } while (quantidade < 1 || quantidade > MAX_CANDIDATOS);
+    }
 
