6d9515c8d01b45a001cb4f76a4bb562344b212c4
larisa Pereira Martins da Silva <larisss.11736372@aluno.mg.gov.br>
2026-09-09 10:42:05 -0300

    feat: listar candidatos disponíveis para votação

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -27,6 +27,16 @@ public class SistemaVotacao {
 
         return indiceEncontrado;
     }
+    static void mostrarCandidatos() {
+        System.out.println("\nCandidatos disponíveis:");
+
+        for (int i = 0; i < quantidadeCandidatos; i++) {
+            System.out.println(
+                    numerosCandidatos[i] + " - " + nomesCandidatos[i]
+            );
+        }
+    }
+
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
