321df2153b202e95b1be1c6f0615c2cdc1fa8b74
larisa Pereira Martins da Silva <larisss.11736372@aluno.mg.gov.br>
2026-09-09 10:38:45 -0300

    feat: concluir cadastro de candidatos

--- src/SistemaVotacao.java
+++ src/SistemaVotacao.java
@@ -120,3 +120,18 @@ public class SistemaVotacao {
         }
     }
 
+        String nome;
+
+        do {
+            System.out.print("Nome do candidato: ");
+            nome = scanner.nextLine().trim();
+
+            if (nome.isEmpty()) {
+                System.out.println("O nome não pode ficar vazio.");
+            }
+        } while (nome.isEmpty());
+
+        numerosCandidatos[i] = numero;
+        nomesCandidatos[i] = nome;
+        votosCandidatos[i] = 0;
+        quantidadeCandidatos++;
